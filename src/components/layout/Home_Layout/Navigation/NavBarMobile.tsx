648800d9ba30eba0ba0a028116ce6c0bcd01da09
Tobiiadee <oluwatobi121@gmail.com>
2024-03-16 11:31:46 +0100

    new fix

--- src/components/layout/Home_Layout/Navigation/NavBarMobile.tsx
+++ src/components/layout/Home_Layout/Navigation/NavBarMobile.tsx
@@ -19,7 +19,7 @@ type ViewType = {
 
 export default function NavBar({ onView }: ViewType) {
   return (
-    <nav onClick={onView} className='fixed top-0 left-0 w-72 z-30 bg-layout-bg rounded-r-lg h-screen py-4 flex flex-col'>
+    <nav onClick={onView} className='fixed top-0 left-0 w-72 z-30 bg-layout-bg rounded-r-lg h-full py-4 flex flex-col'>
       <section className='flex w-full justify-around items-center'>
         <div className='flex gap-1 px-4'>
           <span></span>
@@ -110,7 +110,7 @@ export default function NavBar({ onView }: ViewType) {
         </NavLink>
       </menu>
 
-      <section className='mt-14' id='nav'>
+      <section className='mt-8' id='nav'>
         <NavLink
           to='/learnille/settings'
           className='flex items-center text-sm text-white font-light active:text-black active:bg-background gap-2 px-4 py-3 cursor-pointer transition-all duration-200'>
@@ -129,7 +129,7 @@ export default function NavBar({ onView }: ViewType) {
         </NavLink>
       </section>
 
-      <section className='flex mt-14'>
+      <section className='flex mt-8'>
         <button
           type='button'
           className='bg-white text-xs mx-auto rounded-md shadow-md px-2 py-2 hover:scale-105 transition duration-300'>
@@ -137,7 +137,7 @@ export default function NavBar({ onView }: ViewType) {
         </button>
       </section>
 
-      <section className='flex justify-around items-center border-t border-dotted pt-8 text-sm text-white w-full mt-28'>
+      <section className='flex justify-around items-center border-t border-dotted pt-8 text-sm text-white w-full mt-14'>
         <div className='w-8 h-8 shadow-md ml-2 rounded-full overflow-hidden'>
           <img src='' alt='' />
         </div>
