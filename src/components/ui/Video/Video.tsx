d028aed31a5b3bcaa405e0c2673645c708704ade
Tobiiadee <oluwatobi121@gmail.com>
2024-04-24 09:41:41 +0100

    fix

--- src/components/ui/Video/Video.tsx
+++ src/components/ui/Video/Video.tsx
@@ -9,13 +9,13 @@ type Video_Type = {
 
 export default function Video({ poster }: Video_Type) {
   return (
-    <main className='relative flex items-center w-max h-max overflow-hidden'>
+    <main className='relative flex mx-auto w-full h-max overflow-hidden'>
       <video
         autoPlay={true}
         muted={true}
         poster={poster}
         controls
-        className='h-[20rem] w-[25.5rem] tab:w-[43rem] tab:h-[22rem] nb:w-[34rem] rounded-xl'>
+        className='h-[20rem] w-full tab:w-full tab:h-[22rem]rounded-xl'>
         <source src={video} type='video/mp4' />
       </video>
     </main>
